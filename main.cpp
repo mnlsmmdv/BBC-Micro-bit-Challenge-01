ceae3afe7c4c8fdb5ee3b4fbc3843d0c298c9feb
Ahmed Affaan <71839282+mnlsmmdv@users.noreply.github.com>
2021-12-07 16:39:31 +0500

    Program file description & note updated.
    Program file description and note updated. Also code correctly spaced.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ Author: Ahmed Affaan
 Country: Republic of Maldives
 Language: C++
 File Type: .cpp
+Description: Emotion Badge Program for BBC micro:bit
+Note: -
 //*/
-
-// Emotion Badge Program for BBC micro:bit
 
 #include "MicroBit.h"
 
@@ -31,30 +31,40 @@ int main()
       MicroBitImage happy("0,255,0,255,0\n0,255,0,255,0\n0,0,0,0,0\n255,0,0,0,255\n0,255,255,255,0\n");
 
       uBit.display.print(happy);
+      
     }
 
     //Button B specific LED commands
     if (uBit.buttonB.isPressed())
     {
+      
       MicroBitImage sad("0,255,0,255,0\n0,255,0,255,0\n0,0,0,0,0\n0,255,255,255,0\n255,0,0,0,255\n");
 
       uBit.display.print(sad);
+      
     }
 
     //Button B (second emotion) specific LED commands
     if (uBit.buttonB.isPressed())
     {
+      
       MicroBitImage angry("255,0,0,0,255\n0,255,0,255,0\n0,0,0,0,0\n255,255,255,255,255\n0,255,255,255,0\n");
 
       uBit.display.print(angry);
+      
     }
 
     //Button AB specific LED commands
     if (uBit.buttonAB.isPressed())
     {
+      
       MicroBitImage laugh("0,255,0,255,0\n0,255,0,255,0\n0,0,0,0,0\n255,255,255,255,255\n0,255,255,255,0\n");
 
       uBit.display.print(laugh);
+      
     }
+    
   }
+  
 }
+
